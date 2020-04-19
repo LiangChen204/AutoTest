f240fa32984b6f62733b2e8e01ec10a2955b88be
shutiao <1561114132@qq.com>
2020-04-19 19:57:56 +0800

    test加入extentreports报告

---
+++ myTestng/src/main/java/com/testng/BasicDemo.java
@@ -0,0 +1,15 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author chenliang
+ * @date 2020/4/19
+ */
+public class BasicDemo {
+
+    @Test
+    public void testCase(){
+        System.out.println("测试case");
+    }
+}
---
+++ myTestng/src/main/java/com/testng/TestMethodsDemo.java
@@ -0,0 +1,35 @@
+package com.testng;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+/**
+ * @author chenliang
+ * @date 2020/4/19
+ */
+public class TestMethodsDemo {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1, 2);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa", "aaa");
+    }
+
+    @Test
+    public void logoDemo(){
+        Reporter.log("这个是我们自己写的日志");
+        throw new RuntimeException("这是我自己的运行时异常");
+    }
+
+
+}
