a33a04071c0e7fa8dafd94b6e2cd58c1f532739a
shutiao <1561114132@qq.com>
2020-05-11 14:37:17 +0800

    学习反射相关内容

---
+++ myTestng/src/main/java/com/javabase/util/ReflectClass.java
@@ -0,0 +1,11 @@
+package com.javabase;
+
+/**
+ * @author chenliang
+ * @date 2020/5/8
+ * 封装反射逻辑
+ */
+public class ReflectClass {
+    private final static String TAG = "peter.log.ReflectClass";
+
+}
